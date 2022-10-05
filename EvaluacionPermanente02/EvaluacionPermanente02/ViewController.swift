ebebc8c408fb183a994755790d25526fb2af3366
George Clapham Holguin <63906575+ClaphamGeorgeH@users.noreply.github.com>
2022-10-05 00:33:36 -0400

    Initial commit

---
+++ EvaluacionPermanente02/EvaluacionPermanente02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EvaluacionPermanente02
+//
+//  Created by user213487 on 10/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
